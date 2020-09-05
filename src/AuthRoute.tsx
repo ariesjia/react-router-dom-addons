4e60938c5f9f3730440d5e24fe34033161cc01fd
jiachen <ariesjia00@hotmail.com>
2020-09-05 23:59:43 +0800

    refactor: auth & unauth

--- src/AuthRoute.tsx
+++ src/AuthRoute.tsx
@@ -3,12 +3,30 @@ import AuthenticatedRoute, {
   AuthenticatedRouteProps,
 } from './AuthenticatedRoute'
 
-export const AuthRoute = (props: AuthenticatedRouteProps) => {
-  const { authenticated, ...rest } = props
-  return <AuthenticatedRoute authenticated={authenticated} {...rest} />
+function getAuthRoute(
+  componentName: string,
+  getAuthenticated: (authenticated: boolean) => boolean,
+) {
+  return Object.assign(
+    (props: AuthenticatedRouteProps) => {
+      const { authenticated, ...rest } = props
+      return (
+        <AuthenticatedRoute
+          authenticated={getAuthenticated(authenticated)}
+          {...rest}
+        />
+      )
+    },
+    { displayName: componentName },
+  )
 }
 
-export const UnAuthRoute = (props: AuthenticatedRouteProps) => {
-  const { authenticated, ...rest } = props
-  return <AuthenticatedRoute authenticated={!authenticated} {...rest} />
-}
+export const AuthRoute = getAuthRoute(
+  'AuthRoute',
+  (authenticated) => authenticated,
+)
+
+export const UnAuthRoute = getAuthRoute(
+  'UnAuthRoute',
+  (authenticated) => !authenticated,
+)
